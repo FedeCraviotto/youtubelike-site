262786a2682831f310cd9a5458814bc313d83586
FedeCraviotto <fcraviotto.dev@gmail.com>
2023-01-29 05:06:47 -0300

    commencing ui

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
+
+const Container = styled.div`
+display: flex;
+`;
+
+const Main  = styled.div`
+flex:7
+`;
+const Wrapper  = styled.div``;
+
+function App() {
+  return (
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>
+          Videos      
+        </Wrapper>
+      </Main>
+    </Container>
+      
+    
+  );
+}
+
+export default App;
+
