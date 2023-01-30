2e06e215e000bfbb3360088e5a4dd5ea68880008
FedeCraviotto <fcraviotto.dev@gmail.com>
2023-01-30 18:07:28 -0300

    Styling navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import './navbar.scss';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import VideoCallOutlinedIcon from '@mui/icons-material/VideoCallOutlined';
 function Navbar(){
     return(
         <div className="navbar">
@@ -10,10 +12,14 @@ function Navbar(){
                     <input type="text" placeholder="Search"/>
                     <SearchOutlinedIcon />
                 </div>
-                <button>
-                    <AccountCircleOutlinedIcon/>
-                    Sign In
-                </button>
+                <div className="actions">
+                    <NotificationsOutlinedIcon />
+                    <VideoCallOutlinedIcon />
+                    <button>
+                        <AccountCircleOutlinedIcon/>
+                        Sign In
+                    </button>
+                </div>
             </div>
         </div>
     )
